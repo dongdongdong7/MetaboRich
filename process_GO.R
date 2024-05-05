6eda5c9140aecb5fe9d73589100df07bdabde07a
spd <384220729@qq.com>
2024-05-05 10:07:32 +0800

    update setsList and GO library.

--- process_GO.R
+++ process_GO.R
@@ -9,9 +9,8 @@
 # GO_tibble <- UNIPROT2GO(uniprot_id = uniprot_id)
 # swiss_GO_0.1 <- setsList_protein2setsList_GO(swiss_protein)
 # hmdb_GO <- setsList_protein2setsList_GO(setsList_protein = setsList$hmdb_protein)
-# setsList$swiss_GO_0.9 <- swiss_GO_0.9
-# setsList$swiss_GO_0.1 <- swiss_GO_0.1
-# setsList$hmdb_GO <- hmdb_GO
+# predicted_GO <- swiss_GO_0.9
+# predcited_GO_0.1 <- swiss_GO_0.1
 
 data("setsList")
 data("example_data")
@@ -53,9 +52,4 @@ plot_enrichmentRes(enrichmentRes_kstest, top = 30, plot_type = 2)
 enrichmentRes_grsa <- grsa(input = example_data, enrich_tibble = hmdb_GO, adjust = "fdr", thread = 8)
 enrichmentRes_grsa <- enrichmentRes_grsa %>% dplyr::filter(pvalue < 0.05) %>% dplyr::arrange(pvalue)
 plot_compareRes(list(kstest = enrichmentRes_kstest, grsa = enrichmentRes_grsa), top = 30, plot_type = 2)
-
-# 我们需要看一下IDSL.GOA的对比和GO tree, 是不是要把他们都结合起来
-# IDSL.GOA
-goa_data <- read.table("D:/fudan/Projects/2024/meaWmrn/Progress/database/IDSL.GOA/library/go_to_inchikey14_links.txt", sep = "\t")
-length(unique(goa_data$V1)) # 2393
 
